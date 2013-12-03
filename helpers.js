4c7c56912a9396c06bba523568b8de936156abcd
Ian Hansen <ian@supershabam.com>
2013-12-02 22:55:30 -0700

    split up walk

--- helpers.js
+++ helpers.js
@@ -14,10 +14,22 @@ exports.filter_priority = function(priority) {
   }
 }
 
+exports.group_listeners = function(sorted_listeners) {
+  var result = []
+  var priority
+  var filter
+  while (sorted_listeners.length > 0) {
+    priority = sorted_listeners[0].priority
+    filter   = exports.filter_priority(priority)
+    result.push(L.filter(sorted_listeners, filter))
+    sorted_listeners = L.reject(sorted_listeners, filter)
+  }
+  return result
+}
+
 exports.handle = function(args) {
   return function(listener) {
     return Q().then(function() {
-      console.log('listener', listener, args)
       return listener.listener.apply(null, args)
     })
   }
@@ -43,17 +55,10 @@ exports.unique_events = function(listeners) {
   return L.uniq(L.pluck(listeners, 'evnt'))
 }
 
-exports.walk = function(sorted_listeners, args) {
-  var promise = Q()
-  while (sorted_listeners.length > 0) {
-    var priority  = sorted_listeners[0].priority
-    var filter    = exports.filter_priority(priority)
-    var listeners = L.filter(sorted_listeners, filter)
-    var promises  = L.map(listeners, exports.handle(args))
-    var next      = Q.all(promises)
-    promise       = promise.then(next)
-    sorted_listeners = L.reject(sorted_listeners, filter)
-  }
-  return promise
+exports.walk = function(listeners, args) {
+  var grouped_listeners = exports.group_listeners(listeners.sort(exports.sort_priority))
+  return L.reduce(grouped_listeners, function(memo, listener_group) {
+    return memo.then(Q.all(L.map(listener_group, exports.handle(args))))
+  }, Q())
 }
 
